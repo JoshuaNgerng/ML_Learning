bc82cea73c611c26cb15d98b886932a90501b2b9
JoshuaNgerng <joshuangerng@gmail.com>
2025-04-21 15:15:40 +0800

    start matrix lib

---
+++ matrix_c/include/matrix.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   matrix.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jngerng <jngerng@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/18 23:06:29 by jngerng           #+#    #+#             */
+/*   Updated: 2025/04/19 12:56:04 by jngerng          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <vector>
+#include <iostream>
+#include <tuple>
+#include <cstddef>
+
+using std::vector;
+using std::tuple;
+using std::usize;
+
+template < typename T >
+class Matrix2D {
+	private:
+		vector<T>	arr;
+		tuple<
+	public:
+};
+
+template < typename T >
+class Vector {
+
+};
